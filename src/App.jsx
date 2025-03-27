af1a031e7ceedf664c1379a47a280d06013980a0
akshit-sharma-15 <asharma889452@gmail.com>
2025-03-27 11:43:33 +0530

    done

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,14 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Dashboard from './components/Dashboard';
+import AddMovieForm from './components/Addmovie'
 
 const App = () => {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<Dashboard />} />
+        <Route path='/Add-Movie' element={<AddMovieForm />} />
       </Routes>
     </Router>
   );
